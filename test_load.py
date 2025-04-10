8d8bd605251eaef8268ad33391e96fe2259b2b0e
TruongGiangPham-1 <truonggi@ualberta.ca>
2025-04-10 13:24:33 -0600

    docs: test_load

--- test_load.py
+++ test_load.py
@@ -32,13 +32,15 @@ def main():
     )
     args = parser.parse_args()
 
-    env = registry.make("FourAgentOvercooked-V0")
+    env = registry.make("FourAgentOvercooked-V0")  # the overcooked game engine
     nn = Agent(env).to(device)  # neural network
     nn.load_state_dict(torch.load(args.model_path))
 
-    mappo = MAPPO(env, None, nn, None, None, None)
+    mappo = MAPPO(env, None, nn, None, None, None)  # THE RL AGENT
     obs, info = env.reset() 
     """
+    observvation for each agents, each agent's state vector is sized 404
+
     obs: {
         0: Dict('n_agent_overcooked_features': Box(-inf, inf, (404,), float32)), 
         1: Dict('n_agent_overcooked_features': Box(-inf, inf, (404,), float32)), 
@@ -46,9 +48,11 @@ def main():
         3: Dict('n_agent_overcooked_features': Box(-inf, inf, (404,), float32))
     }
     """
+    # combine the observations of all agents into a single tensor. shape (num_agents, 404)
     state = torch.stack([   torch.FloatTensor(obs[i]['n_agent_overcooked_features']) for i in range(mappo.num_agents)], dim=0).to(device)
 
 
+    # actions for each agents. Each agent action is in the range of 0 to 6
     action = mappo.act(state)
     """
     action: {
