2d901ebf305fa80fe9c63f858d24e438979e3bce
TruongGiangPham-1 <truonggi@ualberta.ca>
2025-05-05 19:10:54 -0600

    cc_script: load opencv, change to python 3.10

--- CC_script.sh
+++ CC_script.sh
@@ -13,10 +13,11 @@
 export results=$SLURM_TMPDIR/results
 export data=$SLURM_TMPDIR/data
 
-module load python/3.12
+module load python/3.10
 module load cuda
+module load gcc opencv/4.9.0
 source /home/truonggi/scratch/MARL/env/bin/activate
 
 echo $1  # run label
 
-python3 main.py --save-path models --num-agents 2 --layout overcooked_cramped_room_v0  --centralised --batch-size 128 --num-minibatches 4 --total-steps 2000000 --seed 2
+python3 main.py --save-path models --num-agents 2 --layout overcooked_cramped_room_v0  --centralised --batch-size 128 --num-minibatches 4 --total-steps 2000000 --seed $1
