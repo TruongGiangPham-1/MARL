a63b2fc7adf91810ce253fbe0b1c47bdebf1bb17
TruongGiangPham-1 <truonggi@ualberta.ca>
2025-07-16 15:35:00 -0600

    fix: CC script positional param beyond 9

--- scripts/CC_script.sh
+++ scripts/CC_script.sh
@@ -27,15 +27,15 @@ echo $6 # ppo_epoch
 echo $7 # clip_param
 echo $8 # value_loss_coef
 echo $9 # entropy_coef
-echo $10 # gamma
-echo $11 # lam
-echo $12 # lr
+echo ${10} # gamma
+echo ${11} # lam
+echo ${12} # lr
 
-echo $13 # data_path
-echo $14 # layout
-echo $15 # feature
+echo ${13} # data_path
+echo ${14} # layout
+echo ${15} # feature
 
 python3 ../main.py --save-path models --num-agents 2 --num-envs $1 --num-steps $2 --num-minibatches $3 \
 --total-steps $4 --seed $5  --ppo-epoch $6 --clip-param $7 \
---value-loss-coef $8 --entropy-coef $9 --gamma $10 --lam $11 --max-grad-norm 0.5 --lr $12 --data-path $13 --layout $14 \
---feature $15
+--value-loss-coef $8 --entropy-coef $9 --gamma ${10} --lam ${11} --max-grad-norm 0.5 --lr ${12} --data-path ${13} --layout ${14} \
+--feature ${15}
