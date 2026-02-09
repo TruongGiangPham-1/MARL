fc85dedfa19dcaf7658546b923d5ea69953d94b1
TruongGiangPham-1 <truonggi@ualberta.ca>
2026-02-09 14:01:34 -0700

    feature: sarsa param tune script

--- scripts/sarsalambda.sh
+++ scripts/sarsalambda.sh
@@ -27,28 +27,3 @@ echo ${15} # feature
 
 # parameter tune
 python3 ../sarsalambda.py --num_episodes $1 --gamma $2 --lambda_ $3 --epsilon $4 --alpha $5 --seed $6 --data_path ${13} --layout ${14} --feature ${15}
-
-
-EPSILON=(0.001 0.005 0.01 0.05 0.1 0.2)
-GAMMA=(0.9 0.95 0.99 0.999)
-LAMBDA=(0.1 0.5 0.7 0.9)
-ALPHA=(0.01 0.05 0.1 0.2 0.3 0.5)
-SEED=(1)
-LAYOUT=("overcooked_cramped_room_v0")
-FEATURE=("Binary_feature")
-
-for epsilon in "${EPSILON[@]}"; do
-    for gamma in "${GAMMA[@]}"; do
-        for lambda in "${LAMBDA[@]}"; do
-            for alpha in "${ALPHA[@]}"; do
-                for seed in "${SEED[@]}"; do
-                    for layout in "${LAYOUT[@]}"; do
-                        for feature in "${FEATURE[@]}"; do
-                            python3 ../sarsalambda.py --num_episodes $1 --gamma $gamma --lambda_ $lambda --epsilon $epsilon --alpha $alpha --seed $seed --data_path ${13} --layout $layout --feature $feature
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
